a9620c675d61501be44362470b3a380268f73d33
wooseungw <woow070840@gmail.com>
2023-09-19 11:55:45 +0900

    132

---
+++ oop/Solution1/week2/week2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "안녕하세요. ";
+    cout << "제 이름은 우승우입니다!" << endl;
+    int buff = 0;
+    cin >> buff ;
+    std::cout << buff ;
+    
+    return 0;
+}   
+
+//endl : 없어도 됨 그냥 한칸 띄워줌
+//
