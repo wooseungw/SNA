ca6bf2797ba4c0aaf0cd1a24e4e689e103b1adba
wooseungw <woow070840@gmail.com>
2023-09-20 14:07:31 +0900

    12335

---
+++ NLP/c3.R
@@ -0,0 +1,29 @@
+setwd("C:/Workspace/SNA/NLP")
+#install.packages("stringr")
+#library("stringr")
+install.packages("ggplot2")
+install.packages("reshape2")
+install.packages("prettyR")
+#library()
+library(prettyR)
+library(ggplot2)
+library(reshape2)
+
+print("Hi")
+a <-c(9,5,51,1,8,1,2,3,2,1,8,1,6,5,1)
+print(1:21)
+print(sum(a))
+print(log(10))
+print(exp(2))
+print(sqrt(21))
+print(Mode(a))
+print(rnorm(10))
+print(sample((15), 12, replace = FALSE, prob = NULL))
+d = 2
+a = d^2
+print(a)
+
+mode(a < d & d <a)
+mode(a)
+y = "홍길동"
+mode(y)
